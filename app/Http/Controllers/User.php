0962f7199dcffdd3fe66415fc799b91d1bf700e2
Peter <peter279k@gmail.com>
2022-07-18 14:18:00 +0800

    Change into 256 variable

--- app/Http/Controllers/User.php
+++ app/Http/Controllers/User.php
@@ -9,13 +9,13 @@ class User extends Controller
 {
     public function checkUser(Request $request)
     {
-        $defaultSha224Password = '34250003024812';
+        $defaultSha256Password = '34250003024812';
         $defaultBcryptPassword = '123456789012345678901234567890123456789012345678901234567890123456789012';
         $user = $request->input('user');
         $password = $request->input('password');
-        if ($password === $defaultSha224Password) {
-            $resEnMessage = sprintf('It seems that you use the default password: %s. The login system doesn\'t like the default password. Please figure out another way :).', $defaultSha224Password);
-            $resTwMessage = sprintf('登入系統不太喜歡預設密碼: %s，請試著想出其他方法登入 :)', $defaultSha224Password);
+        if ($password === $defaultSha256Password) {
+            $resEnMessage = sprintf('It seems that you use the default password: %s. The login system doesn\'t like the default password. Please figure out another way :).', $defaultSha256Password);
+            $resTwMessage = sprintf('登入系統不太喜歡預設密碼: %s，請試著想出其他方法登入 :)', $defaultSha256Password);
             return json_encode([
                 'result' => $resEnMessage,
                 'result_tw' => $resTwMessage,
