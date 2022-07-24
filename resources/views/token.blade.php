148afb671e13b797e4553dada1f6ac28975fbf2e
Peter <peter279k@gmail.com>
2022-07-24 14:49:14 +0800

    Remove customized CSS for Bootstrap

--- resources/views/token.blade.php
+++ resources/views/token.blade.php
@@ -14,26 +14,6 @@
     <!-- Favicons -->
 <link rel="icon" href="https://coscup.org/2020/images/banner-logo.svg" type="image/svg+xml">
 <meta name="theme-color" content="#7952b3">
-
-
-    <style>
-      .bd-placeholder-img {
-        font-size: 1.125rem;
-        text-anchor: middle;
-        -webkit-user-select: none;
-        -moz-user-select: none;
-        user-select: none;
-      }
-
-      @media (min-width: 768px) {
-        .bd-placeholder-img-lg {
-          font-size: 3.5rem;
-        }
-      }
-    </style>
-    
-    <!-- Custom styles for this template -->
-    <link href="https://getbootstrap.com/docs/5.0/examples/sign-in/signin.css" rel="stylesheet">
   </head>
   <body class="text-center">
     
